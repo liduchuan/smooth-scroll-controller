dcd9ef0c2b90a42248fe35d823f4dac84ab949e7
liduchuan <alex.liduchuan@gmail.com>
2025-12-01 15:59:05 +0800

    refactor: remove unused components and styles for a cleaner codebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return <div>123</div>;
+}
+
+createRoot(document.getElementById("root")).render(<App />);
